b280ffcc985021aaa9283fb2387ce94e377ea562
goyalk130 <goyalkaran130@gmail.com>
2023-06-27 22:27:22 +0530

    chnaged

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,37 +1,36 @@
-import { useSession } from 'next-auth/client';
 import { getPosts } from '../../../utils/blogger';
 
-export default function Blog({ posts }) {
-  const [session] = useSession();
+export default function Blog() {
+  // const [session] = useSession();
 
-  if (!session) {
-    return <div>Please sign in to access the blog.</div>;
-  }
+  // if (!session) {
+  //   return <div>Please sign in to access the blog.</div>;
+  // }
 
   return (
     <div>
       <h1>Welcome to the Blog</h1>
-      {posts.map((post) => (
+      {/* {posts.map((post) => (
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.content}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  let posts = [];
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+//   let posts = [];
 
-  if (session && session.accessToken) {
-    posts = await getPosts(session.accessToken);
-  }
+//   if (session && session.accessToken) {
+//     posts = await getPosts(session.accessToken);
+//   }
 
-  return {
-    props: {
-      posts,
-    },
-  };
-}
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// }
